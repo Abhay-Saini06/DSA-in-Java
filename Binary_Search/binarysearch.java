aaf8357598fae0c1adf603c5253ee4ccee69b875
Abhay-Saini-06 <siddsaini0906@gmail.com>
2026-05-15 14:01:29 +0530

    first commit

---
+++ Binary_Search/binarysearch.java
@@ -0,0 +1,39 @@
+package Binary_Search;
+import java.util.Scanner;
+import java.util.Arrays;
+public class binarysearch{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target element to search:");
+        int target = sc.nextInt();
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
