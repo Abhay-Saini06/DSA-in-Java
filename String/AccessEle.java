ada6ad2c11b5db299b801aa64522e139b174c793
Abhay-Saini-06 <siddsaini0906@gmail.com>
2026-05-23 12:01:53 +0530

    updated files

---
+++ String/AccessEle.java
@@ -0,0 +1,12 @@
+package String;
+
+public class AccessEle {
+    public static void main(String[] args){
+        String s = "Hello World";
+        System.out.println(s.charAt(0));
+        System.out.println(s.charAt(1));
+        System.out.println(s.charAt(2));
+        System.out.println(s.charAt(3));
+        System.out.println(s.charAt(4));
+    }
+}
